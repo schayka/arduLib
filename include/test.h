cc98980d8a660f453d868972104c1af1450e23b5
Станислав Чайка <stas@MacBook-Air-Stanislav.local>
2024-06-29 21:16:02 +0300

    add test lib

---
+++ include/test.h
@@ -0,0 +1,17 @@
+//
+// Created by Станислав Чайка on 29.06.2024.
+//
+
+#ifndef ARDULIB_TEST_H
+#define ARDULIB_TEST_H
+
+
+class test {
+    public:
+    test();
+    ~test();
+    void demo();
+};
+
+
+#endif //ARDULIB_TEST_H
---
+++ src/libs/test/test.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Станислав Чайка on 29.06.2024.
+//
+
+#include "../../../include/test.h"
+
+test::test() {}
+test::~test() {}
+void test::demo() {}
